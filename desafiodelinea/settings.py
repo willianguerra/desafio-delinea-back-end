5fa53a62bc788d1349d3dfb1c1b9b1fba6d88441
Willian Guerra Possamai <47372963+willianguerra@users.noreply.github.com>
2022-08-02 23:41:25 -0300

    fix: :bug: adjusted time token

--- desafiodelinea/settings.py
+++ desafiodelinea/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import django_heroku 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,5 +135,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes=180),
+}
 
 django_heroku.settings(locals())
