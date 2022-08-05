a34260c6d8951a3242bb7a16c57721e740874a9e
Willian Guerra Possamai <47372963+willianguerra@users.noreply.github.com>
2022-08-04 21:18:06 -0300

    fix: :hammer: adjusted url users

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.views.decorators.csrf import csrf_exempt
 from .views import CreateUserView
 
 urlpatterns = [
-    path('cadastro', csrf_exempt(CreateUserView.as_view())),
+    path('user', csrf_exempt(CreateUserView.as_view())),
 ]
